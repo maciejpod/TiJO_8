5bf3f90a8dace71b64cd4e5627bcfd1db122e7b9
m.podolanski <m.podolanski@gmail.com>
2017-04-28 08:32:57 +0200

    mutation exercise part 1

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,17 +3,21 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-describe('reverseNumber', function ()
-{
-
+describe('reverseNumber', function () {
+    it('should return string with reversed numbers', function () {
+        expect(app.reverseNumber(1234)).to.eql(4321);
+    });
 });
 
-describe('returnOnlyLetter', function ()
-{
-
+describe('returnOnlyLetter', function () {
+    it('should return string which conatins only letters', function () {
+        expect(app.returnOnlyLetter('12345ap')).to.eql('ap');
+    });
 });
 
-describe('isEmail', function ()
-{
-
+describe('isEmail', function () {
+    it('should return false for non email string', function () {
+        expect(app.isEmail("alamakota")).to.eql(false);
+    })
 });
+
